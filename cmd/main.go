33e044f8fa34888596e3e29bfe6c35dedf8c2d4c
Vinicius <viniciusmartinsdacunha1@gmail.com>
2024-06-23 00:52:22 -0300

    fix(redis):redis credentials

--- cmd/main.go
+++ cmd/main.go
@@ -62,8 +62,9 @@ func main() {
 	}
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr: os.Getenv("REDIS_URL"),
-		Password: "",
+		Addr: os.Getenv("REDIS_HOST") + ":" + os.Getenv("REDIS_PORT"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		Username: os.Getenv("REDIS_USER"),
 		DB: 0,
 	})
 
